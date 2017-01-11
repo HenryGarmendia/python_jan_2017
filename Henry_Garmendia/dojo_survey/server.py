c5b9f6d1e38e28ab05259025eed0800e401cd12b
HenryGarmendia <nsaneznicoyaz@Henrys-MacBook-Pro.local>
2017-01-10 23:50:26 -0600

    Assignment:_Dojo_Survey_1.10_11.49

---
+++ Henry_Garmendia/dojo_survey/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html', name='Henry Garmendia')
+
+@app.route('/result')
+def result():
+	return render_template('result.html')
+
+@app.route('/users', methods=['POST'])
+def create_user():
+	print "Got Post Info"
+	# we'll talk about the following two lines after we learn a little more
+	# about forms
+	fullName = request.form['full_name']
+	cityLoc = request.form['location']
+	favLang = request.form['fav-track']
+	textArea = request.form['areatext']
+	# redirects back to the '/' route
+	return render_template('result.html', full_Name=fullName, city_Loc=cityLoc, fav_Lang=favLang, comment=textArea)
+
+app.run(debug=True)  # run our server
